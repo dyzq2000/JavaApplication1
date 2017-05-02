c8ba67c98bf47cda80f1e06aef981839ee88303b
dyzq2000 <zden28@student.monash.edu>
2017-05-02 11:40:53 +1000

    first upload

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,17 @@
+package javaapplication1;
+
+public class JavaApplication1 
+{
+
+    public static void main(String[] args) 
+    {
+        JavaApplication1.printf(i);
+    }
+    public static void printf(int i)
+    {
+        for(i=0;i<10;i++)
+        {
+            System.out.println(++i);
+        }
+    }      
+}
